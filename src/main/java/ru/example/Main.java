dc09488b4369efe313c34fd113c4c1421a438dbb
mskaleganov <mkaleganov@mail.ru>
2020-11-04 17:13:20 +0300

    home work 1

--- src/main/java/ru/example/Main.java
+++ src/main/java/ru/example/Main.java
@@ -30,14 +30,17 @@ public class Main {
         //System.exit(2);
     }
 
+    //Написать метод принимающий 1 параметр типа int и возвращающий его представление в виде печатного символа
     private static char NumbInChar(int a7) {
         return (char) a7;
     }
 
+    //Написать метод принимающий 1 параметр типа char и возвращающий его числовое представление.
     private static int CharInNumb(char a6) {
         return a6;
     }
 
+    //Написать метод выводящий бинарное представление Integer.MAX_VALUE и Integer.MAX_VALUE + 1. Для вывода бинарного представления использовать Integer.toBinaryString(). Без входных параметров и не возвращающая ничего. Покажет, что переполнение не вызывает ошибку и как это выглядет в памяти
     private static void PrintMAX_VALUE() {
         int max1 = Integer.MAX_VALUE;
         //long max2 = max1 + 1;
@@ -45,6 +48,7 @@ public class Main {
         System.out.println("Максимальное число типа Integer + 1 = " + (max1 + 1) + " (dec) = " + Integer.toBinaryString(max1 + 1) + " (bin)");
     }
 
+    //Написать метод выводящий на экран в цикле бинарное представление чисел от 0 до 30. Для вывода бинарного представления использовать Integer.toBinaryString(). Без входных параметров и не возвращающая ничего.
     private static void isBinAt0To30CharNumber() {
         for (int i = 1; i <= 30; i++) {
             System.out.print(i + " = " + Integer.toBinaryString(i) + " ; ");
@@ -52,14 +56,17 @@ public class Main {
         System.out.println();
     }
 
+    //Написать метод isCharNumber принимающий 1 параметр типа char. Если передано число, то вернуть true, а в остальных случаях false
     private static boolean isCharNumber(char a5) {
         return Character.isDigit(a5);
     }
 
+    //Написать метод isCharA принимающий 1 параметр типа char. Если передана буква 'А', то возвращать true, а в остальных случаях false
     private static boolean isCharA(char a4) {
         return (a4 == 'A') || ((a4 == 'А'));
     }
 
+    //Написать метод getMaxNumber принимающий 2 параметра: int и int. Метод должен сравнить переданные параметры между собой и вернуть максимальный. Для решения необходимо использовать IF или тернарный оператор ?:
     private static int getMaxNumber(int a3, int b3) {
         //if (a3<b3) return b3; else return a3;//С помощью  IF
         //return (a3 >= b3) ? a3 : b3; //С помощью тернарного оператора
@@ -67,10 +74,12 @@ public class Main {
 
     }
 
+    //Написать публичный метод принимающий 2 параметра: первый типа byte и второй типа short. Метод должен возвращать сумму значений параметров. Тип возвращаемого значения должен быть byte.
     private static byte summa(byte a1, short b1) {
         return (byte) (a1 + b1);
     }
 
+    //Написать публичный метод принимающий 2 параметра: первый типа int и второй типа long. Метод должен возвращать произведение значений параметров. Тип возвращаемого значения int.
     private static int proizvedenie(int a2, long b2) {
         return (int) (a2 * b2);
     }
